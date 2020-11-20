df02d1a71392fe9e4b78b61258701be921b0f983
sunhao <1079427442@qq.com>
2020-11-20 13:52:27 +0800

    update_seckill_003_jmeter_001

--- seckill/seckill/src/main/java/com/seckill/controller/DemoController.java
+++ seckill/seckill/src/main/java/com/seckill/controller/DemoController.java
@@ -1,15 +1,19 @@
 package com.seckill.controller;
 
+import com.seckill.pojo.SeckillUser;
+import com.seckill.result.Result;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/Demo")
+@RequestMapping("/User")
 public class DemoController {
-    @RequestMapping("/hello")
+    @RequestMapping("/info")
     @ResponseBody
-    public String hello(){
-        return "hello world!";
+    public Result<SeckillUser> info(Model model,SeckillUser user){
+        model.addAttribute("user",user);
+        return Result.success(user);
     }
 }
